e6c77ad3a524727de85d3e7be07617c2944e6a5f
Yashiro-SST <67620182+Yashiro-SST@users.noreply.github.com>
2020-06-30 23:54:41 +0900

    Update darden.f90

--- darden/darden.f90
+++ darden/darden.f90
@@ -15,7 +15,7 @@ program darden
   real(8) ita_max
   real(8) mach_ang
   real(8) lam1, lam2, lam3
-  integer  dn
+  integer dn
 
 
   !---Read paramator---!
@@ -89,7 +89,6 @@ module daikei_sekibun
       
       real(8), intent(in) :: l, yf, yr, A
       integer, intent(in) :: dn
-
       real(8) dx, x, y, sum, FYR_int1
       integer i, n
 
@@ -145,7 +144,6 @@ module daikei_sekibun
       
       real(8), intent(in) :: l, yf, yr, lam, B, C0
       integer, intent(in) :: dn
-
       real(8) dx, x, y, sum, FYR_int3
       integer i, n
 
@@ -154,7 +152,7 @@ module daikei_sekibun
       n = nint((lam - yf) / dx)
 
       do i = 0, n
-        x = dble(nint((lam - yf))) + dx * dble(i)
+        x = dble(nint(yf)) + dx * dble(i)
         y = sqrt(abs(l - x)) * (B * (x - yf) + C0) / (yr - x)
 
         if (i == 0 .or. i ==n) then
@@ -173,7 +171,6 @@ module daikei_sekibun
       
       real(8), intent(in) :: l, yf, yr, lam, B, D0
       integer, intent(in) :: dn
-
       real(8) dx, x, y, sum, FYR_int4
       integer i, n
 
@@ -182,7 +179,7 @@ module daikei_sekibun
       n = nint((l - lam) / dx)
 
       do i = 0, n
-        x = dble(nint((l - lam))) + dx * dble(i)
+        x = dble(nint(lam)) + dx * dble(i)
         y = sqrt(abs(l - x)) * (B * (x - yf) + D0) / (yr - x) 
 
         if (i == 0 .or. i ==n) then
@@ -209,7 +206,7 @@ module daikei_sekibun
     
     end function F10
 
-    function FYR_percialC1(l, yf, yr, A, C0, S, mu, dn) Result(FYR_C1)
+    function FYR_partialC1(l, yf, yr, A, C0, S, mu, dn) Result(FYR_C1)
 
       real(8), intent(in) :: l, yf, yr, A, C0, S, mu
       integer, intent(in) :: dn
@@ -222,7 +219,7 @@ module daikei_sekibun
 
       do i = 0, n
         x = dx * dble(i)
-        y = 2.0d0 * sqrt(abs(l - x)) / (yf * (yr - x)) &
+        y = - 2.0d0 * x * sqrt(abs(l - x)) / (yf * (yr - x)) &
             * (A / (S * mu *(yr - x)) -(2.0d0 * C0 / (S * yf) - 0.50d0))
 
         if (i == 0 .or. i ==n) then
@@ -232,21 +229,106 @@ module daikei_sekibun
         end if
       enddo
 
-      FYR_C1 = - sum
+      FYR_C1 = sum
 
-    end function FYR_percialC1
+    end function FYR_partialC1
 
-    !function FYR_C2
-    !end function FYR_C2
+    function FYR_partialC2(l, yf, yr, A, C0, S, mu, dn) Result(FYR_C2)
 
-    !function FYR_C3
-    !end function FYR_C3
+      real(8), intent(in) :: l, yf, yr, A, C0, S, mu
+      integer, intent(in) :: dn
+      real(8) dx, x, y, sum, FYR_C2
+      integer i, n
 
-    !function FYR_C4
-    !end function FYR_C4
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((yf / 2.0d0) / dx)
 
-    !function FYR_Csum
-    !end function FYR_Csum
+      do i = 0, n
+        x = dble(nint(yf / 2.0d0)) + dx * dble(i)
+        y = - sqrt(abs(l - x)) / (yr - x) * (1.0d0 / (S * mu * (yr - x)) &
+            * (C0 * (2.0d0 * x / yf - 1.0d0) - A * (2.0d0 * x / yf - 2.0d0)) &
+            + (2.0d0 * x / yf - 1.0d0) - (2.0d0 * C0 / (S * yf) - 0.50d0) &
+            * (2.0d0 * x / yf - 2.0d0))
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      FYR_C2 = sum
+
+    end function FYR_partialC2
+
+    function FYR_partialC3(l, S, yf, yr, lam, B, C0, mu, dn) Result(FYR_C3)
+
+      real(8), intent(in) :: l, S, yf, yr, lam, B, C0, mu
+      integer, intent(in) :: dn
+      real(8) dx, x, y, sum, FYR_C3
+      integer i, n
+
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((lam - yf) / dx)
+
+      do i = 0, n
+        x = dble(nint(yf)) + dx * dble(i)
+        y = - sqrt(abs(l - x)) / (yr - x) * ((B * (x - yf) + C0) &
+            / (S * mu * (yr - x)) + 1.0d0)
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      FYR_C3 = sum
+
+    end function FYR_partialC3
+
+    function FYR_partialC4(l, S, yf, yr, lam, B, D0, mu, dn) Result(FYR_C4)
+      
+      real(8), intent(in) :: l, S, yf, yr, lam, B, mu, D0
+      integer, intent(in) :: dn
+      real(8) dx, x, y, sum, FYR_C4
+      integer i, n
+
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((l - lam) / dx)
+
+      do i = 0, n
+        x = dble(nint(lam)) + dx * dble(i)
+        y = sqrt(abs(l - x)) / (S * mu * (yr - x)**2.0d0) * (B * (x - yf) + D0)  
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      FYR_C4 = sum
+
+    end function FYR_partialC4
+
+    function FYR_partialC(l, S, yr, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
+      FYR_C1, FYR_C2, FYR_C3, FYR_C4) Result(FYRC)
+      
+      real(8), intent(in) :: l, S, yr, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
+      FYR_C1, FYR_C2, FYR_C3, FYR_C4
+      real(8) pi, FYRC
+
+      pi = 2.0d0 * acos(0.0d0)
+
+      FYRC = sqrt(S * mu) * C0**(-1.50d0) &
+            * (FYR_int1 + FYR_int2 + FYR_int3 + FYR_int4) / 2.0d0 * pi &
+            + (FYR_C1 + FYR_C2 + FYR_C3 + FYR_C4) / (pi * sqrt(yr - l))
+
+    end function FYR_partialC
 
     !function FYR_D
     !end function FYR_D
