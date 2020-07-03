1611794a56ebbc6622d3c4839994e52256d89012
Yashiro-SST <67620182+Yashiro-SST@users.noreply.github.com>
2020-07-04 01:38:58 +0900

    0703midnight

--- darden/darden.f90
+++ darden/darden.f90
@@ -7,21 +7,25 @@ program darden
   use daikei_sekibun
   implicit none
 
+  !---Variable definition---!
+
   real(8) A, C, D, lam, yr, m
   real(8) B, mu, yf, W, gamma, M_inf, h_inf, l, R, T_inf, rho_inf, AMW
   real(8) S, k, beta, u_inf, a_inf, Ae_l
   real(8) C0,dC,D0,dD
   real(8) err
   real(8) F1, F2, delta
-  real(8) ita_max
   real(8) mach_ang
   real(8) lam1, lam2, lam3
   real(8) dx
+  real(8) F10, F20, F1C, F2C, F1D, F2D
+  real(8) FYR_int1, FYR_int2, FYR_int3, FYR_int4
+  real(8) FYR_C1, FYR_C2, FYR_C3, FYR_C4
+  real(8) Q1, Q2, Q3, Q4
   real(8), allocatable :: x_array(:), F(:), Ae(:)
-  integer dn
+  integer dn, i, ite_max
 
-
-  !---Read paramator---!
+  !---Reading paramator---!
   open(10,file='input_darden.txt')
 
   read(10,*) mu, B               !---front/rear shock ratio, sig paramata B
@@ -35,7 +39,7 @@ program darden
 
   close(10)
 
-  !---Calculate Known function---!
+  !---Calculation Known function---!
 
   beta = sqrt(abs(M_inf**2.0 - 1.0))
   a_inf = sqrt(abs(gamma * R * T_inf / (AMW * 0.001)))
@@ -43,13 +47,6 @@ program darden
   k = (gamma + 1) * M_inf**2.0 / sqrt(abs(2.0 * beta**3.0))
   S = l / (k * sqrt(abs(h_inf / l)))
   Ae_l = beta * W /(rho_inf * u_inf**2.0)
-
-  dx = l / dble(dn)
-  aloocate (x(dn))
-  do i = 0, n
-    x(i) = dx * dble(i)
-  enddo
-
 
   !---Output---!
 
@@ -73,36 +70,64 @@ program darden
   write(*,*) 'Ae_l = ', Ae_l
   write(*,*) '32.0d0, 32d0 ', 32.0d0, 32d0
 
+  dx = l / dble(dn)
+  allocate (x_array(dn))
+  do i = 0, dn
+    x_array(i) = dx * dble(i)
+  enddo
+
   !---Newton method---!
-
-
   do i = 0, ite_max
 
-
-    !---Cal unknown funcion---!
-  
+    !---Calculation unknown funcion---!
     A = ( C0**2.0d0 / ( S * yf ) ) - C0 / 2.0d0
     yr = l + C0 / ( S * mu )
     lam1 = 32.0d0 * A * (l**2.5) / 15.0 * yf
     lam2 = 32.0d0 * (C0 - 2.0d0 * A) * (l - yf / 2.0d0) ** 2.5d0 / (15.0 * yf)
     lam3 = 16.0d0 * (B - 2.0d0 * (C0 - A) / yf) * (l - yf) ** 2.5d0
-    lam = l - (3.0d0 * ( lam1 + lam2 + lam3 - Ae_l  ) / ((8,0d0 * (C0 + D0) ) ** (2.0d0 /3.0d0)))
+    lam = l - (3.0d0 * ( lam1 + lam2 + lam3 - Ae_l  ) / ((8.0d0 * (C0 + D0) ) ** (2.0d0 /3.0d0)))
 
-    F10 = F10()
-    F20 = F20()
-    F1C = F1C()
-    F2C = F2C()
-    F1D = F1D()
-    F2D = F2D()
+    !---Calculation F10, F20, F1 partial CorD, and F2 partial CorD---!
 
-    dC = (F2D * F10 - F1D * F20) / (F1C * F2D - F1D * F2C)
-    dD = (-F2C * F10 + F1C * F20) / (F1C * F2D - F1D * F2C)
+    FYR_int1 = FYR_integral1(l, yf, yr, A, dn)
+    FYR_int2 = FYR_integral2(l, yf, yr, A, C0, dn)
+    FYR_int3 = FYR_integral3(l, yf, yr, lam, B, C0, dn)
+    FYR_int4 = FYR_integral4(l, yf, yr, lam, B, D0, dn)
+    F10 = F1initial(l, S, yr, B, D0, FYR_int1, FYR_int2, FYR_int3, FYR_int4)
+
+    FYR_C1 = FYR_partialC1(l, yf, yr, A, C0, S, mu, dn)
+    FYR_C2 = FYR_partialC2(l, yf, yr, A, C0, S, mu, dn)
+    FYR_C3 = FYR_partialC3(l, S, yf, yr, lam, B, C0, mu, dn)
+    FYR_C4 = FYR_partialC4(l, S, yf, yr, lam, B, D0, mu, dn)
+    F1C = F1_partialC(l, S, yr, B, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
+                      FYR_C1, FYR_C2, FYR_C3, FYR_C4)
+    
+    F1D = F1_partialD(l, yr, lam, dn)
+
+    Q1 = Qterm1(l, yf, yr, A, dn)
+    Q2 = Qterm2(l, yf, yr, A, C0, dn)
+    Q3 = Qterm3(l, yf, yr, lam, B, C0, dn)
+    Q4 = Qterm4(l, yf, yr, lam, B, D0, dn)
+    F20 = F2initial(l, S, yr, B, D0, Q1, Q2, Q3, Q4)
+
+    F2C = F2_partialC()
+    
+    F2D = F2_partialD()
+
+    !---calculation delC and delD @ this iteration---!
+
+    dC = - (F2D * F10 - F1D * F20) / (F1C * F2D - F1D * F2C)
+    dD = - (-F2C * F10 + F1C * F20) / (F1C * F2D - F1D * F2C)
 
     !---Convergence judgment---!
 
-    if (dC < err .and. dD < err) exit
+    if (dC < err .and. dD < err) then
+      write(*,*) 'delD and delC is Converged!!!'
+      write(*,*) 'delC =', dC
+      write(*,*) 'delD =', dD
+      write(*,*) 'Iteration number =', i
 
-    if (dC >= err .and. dD >= err) then
+    else if (dC >= err .and. dD >= err) then
       C0 = C0 + dC
       D0 = D0 + dD
     
@@ -123,18 +148,15 @@ program darden
 
   !---Culculate Equivalent Area---!
 
-  Ae_cal()
-
-
-
+  Ae_cal(l, A, B, C, D, yf, dn)
 
 
 
 end program darden
 
 
-!---module function---!
 
+!---module function of F1, F2---!
 
 module daikei_sekibun
   implicit none
@@ -245,17 +267,17 @@ module daikei_sekibun
 
     end function FYR_integral4
 
-    function F10(l, S, yr, B, D0, FYR_int1, FYR_int2, FYR_int3, FYR_int4) Result(F1ini)
+    function F1initial(l, S, yr, B, D0, FYR_int1, FYR_int2, FYR_int3, FYR_int4) Result(F10)
 
       real(8), intent(in) :: l, S, yr, B, D0, FYR_int1, FYR_int2, FYR_int3, FYR_int4
-      real(8) FYR, pi, F1ini
+      real(8) FYR, pi, F10
 
       pi = 2.0d0 * acos(0.0d0)
       
       FYR = -(FYR_int1 + FYR_int2 + FYR_int3 + FYR_int4) / (pi * sqrt(abs(yr - l)))
-      F1ini = S * (yr - l) + B * (l - yr) - D0 - FYR
+      F10 = S * (yr - l) + B * (l - yr) - D0 - FYR
     
-    end function F10
+    end function F1initial
 
     function FYR_partialC1(l, yf, yr, A, C0, S, mu, dn) Result(FYR_C1)
 
@@ -366,12 +388,12 @@ module daikei_sekibun
 
     end function FYR_partialC4
 
-    function FYR_partialC(l, S, yr, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
-      FYR_C1, FYR_C2, FYR_C3, FYR_C4) Result(FYRC)
+    function F1_partialC(l, S, yr, B, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
+      FYR_C1, FYR_C2, FYR_C3, FYR_C4) Result(F1C)
       
-      real(8), intent(in) :: l, S, yr, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
+      real(8), intent(in) :: l, S, yr, B, mu, C0, FYR_int1, FYR_int2, FYR_int3, FYR_int4, &
       FYR_C1, FYR_C2, FYR_C3, FYR_C4
-      real(8) pi, FYRC
+      real(8) pi, FYRC, F1C
 
       pi = 2.0d0 * acos(0.0d0)
 
@@ -379,28 +401,158 @@ module daikei_sekibun
             * (FYR_int1 + FYR_int2 + FYR_int3 + FYR_int4) / 2.0d0 * pi &
             + (FYR_C1 + FYR_C2 + FYR_C3 + FYR_C4) / (pi * sqrt(yr - l))
 
-    end function FYR_partialC
+      F1C = 1.0d0 / mu + B / (S * mu) - FYRC 
 
-    !function F1_partialD
-    !end function FYR_partialD
+    end function F1_partialC
 
-    !function Q1
-    !end function Q_C1
+    function F1_partialD(l, yr, lam, dn) Result(F1D)
 
-    !function Q2
-    !end function Q_C1
+      real(8), intent(in) :: l, yr, lam
+      integer, intent(in) :: dn
+      real(8) dx, x, y, sum, F1D
+      integer i, n
 
-    !function Q3
-    !end function Q_C1
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((l - lam) / dx)
 
-    !function Q4
-    !end function Q_C1
+      do i = 0, n
+        x = dble(nint(lam)) + dx * dble(i)
+        y = sqrt(abs(l - x)) / (yr - x) 
 
-    !function Q_sum
-    !end function Q_C1
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
 
-    !function F20
-    !end function F20
+      F1D = - 1.0d0 - sum
+
+    end function F1_partialD
+
+    function Qterm1(l, yf, yr, A, dn) result(Q1)
+      
+      real(8), intent(in) :: l, yf, yr, A
+      integer, intent(in) :: dn
+      real(8) dx, x, y, t, sum, pi, Q1
+      integer i, n
+
+      pi = 2.0d0 * acos(0.0d0)
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((yf / 2.0d0) / dx)
+
+      do i = 0, n
+        x = dx * dble(i)
+        t = atan(sqrt((yr - l) / (l - x)))
+        y = 2.0d0 * x / yf * A * t
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      Q1 = - 2.0d0 / pi * sum
+
+    end function Qterm1
+
+    function Qterm2(l, yf, yr, A, C0, dn) result(Q2)
+      
+      real(8), intent(in) :: l, yf, yr, A, C0
+      integer, intent(in) :: dn
+      real(8) dx, x, y, t, sum, pi, Q2
+      integer i, n
+
+      pi = 2.0d0 * acos(0.0d0)
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((yf / 2.0d0) / dx)
+
+      do i = 0, n
+        x = dble(nint(yf / 2.0d0)) + dx * dble(i)
+        t = atan(sqrt((yr - l) / (l - x)))
+        y = (C0 * (2.0d0 * x / yf - 1.0d0) - A * (2.0d0 * x / yf -2.0d0) ) * t
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      Q2 = - 2.0d0 / pi * sum
+
+    end function Qterm2
+
+    function Qterm3(l, yf, yr, lam, B, C0, dn) Result(Q3)
+      
+      real(8), intent(in) :: l, yf, yr, lam, B, C0
+      integer, intent(in) :: dn
+      real(8) dx, x, y, t, sum, pi, Q3
+      integer i, n
+
+      pi = 2.0d0 * acos(0.0d0)
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((lam - yf) / dx)
+
+      do i = 0, n
+        x = dble(nint(yf)) + dx * dble(i)
+        t = atan(sqrt((yr - l) / (l - x)))
+        y = (B * (x - yf) + C0) * t
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      Q3 = - 2.0d0 / pi * sum
+
+    end function Qterm3
+
+    function Qterm4(l, yf, yr, lam, B, D0, dn) Result(Q4)
+      
+      real(8), intent(in) :: l, yf, yr, lam, B, D0
+      integer, intent(in) :: dn
+      real(8) dx, x, y, t, sum, pi, Q4
+      integer i, n
+
+      pi = 2.0d0 * acos(0.0d0)
+      dx =  l / dble(dn) 
+      sum = 0.0d0
+      n = nint((l - lam) / dx)
+
+      do i = 0, n
+        x = dble(nint(lam)) + dx * dble(i)
+        t = atan(sqrt((yr - l) / (l - x)))
+        y = (B * (x - yf) + D0) * t
+
+        if (i == 0 .or. i ==n) then
+          sum = sum + 0.5d0 * y
+        else
+          sum = sum + y
+        end if
+      enddo
+
+      Q4 = - 2.0d0 / pi * sum
+
+    end function Qterm4
+
+    function F2initial(l, S, yr, B, D0, Q1, Q2, Q3, Q4) Result(F20)
+
+      real(8), intent(in) :: l, S, yr, B, D0, Q1, Q2, Q3, Q4
+      real(8) Q_sum, F20
+      
+      Q_sum = Q1 + Q2 + Q3 + Q4
+      F20 = (B * (l - yr) - D0 + S / 2.0d0 *(yr - D0)) * (yr - l) - Q_sum
+
+    end function F2initial
+
 
     !function Q_partialC1
     !end function Q_partialC1
@@ -423,19 +575,14 @@ module daikei_sekibun
     !function F2_partialD
     !end function F2_D
 
-    function Ae_cal(l, A, B, C, D, yf, dn) Result(Ae)
-      integer, intent(in) :: dn
-      real(8), intent(in) :: l, A, B, C, D, yf
-      real(8) Ae(n)
+    !function Ae_cal(l, A, B, C, D, yf, dn) Result(Ae)
+      !integer, intent(in) :: dn
+      !real(8), intent(in) :: l, A, B, C, D, yf
+      !real(8) Ae(n)
 
-      dy = 
+      !dy = 0.0d0
 
-    
-    end function Ae_cal
-
-!!!0630!!
-!!!06/30 17:09!!!
-!!!06/30 17:14!!!
+    !end function Ae_cal
 
 
 end module daikei_sekibun
