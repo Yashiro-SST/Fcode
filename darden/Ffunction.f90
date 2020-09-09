7a8127d6e7b36bfaba88ee15c16921bc802b0142
Yashiro-SST <67620182+Yashiro-SST@users.noreply.github.com>
2020-09-09 19:27:24 +0900

    Update Ffunction.f90

--- darden/Ffunction.f90
+++ darden/Ffunction.f90
@@ -3,6 +3,75 @@
 !	input	 :	input_darden.txt										!
 !	output	:	F.txt, AE.txt												!
 !=======================================================================!
+module Ffunc
+  implicit none
+  contains
+
+    function cal_Ffunc(x, l, A, B, C, D, yf, lam, dn) result(F)
+      integer, intent(in) :: dn
+      real(8), intent(in) :: x(0:dn)
+      real(8), intent(in) :: l, A, B, C, D, yf, lam 
+      real(8) F(0:dn), dx
+      integer i
+
+      F(:) = 0.0d0
+      dx = l / dble(dn)
+    
+      do i = 0, dn
+        if(x(i) <= (yf / 2.0d0)) then
+          F(i) = 2.0d0 * x(i) * A / yf
+        
+        else if((yf / 2.0d0) < x(i) .and. x(i) < yf ) then
+          F(i) = C * (2.0d0 * x(i) / yf - 1.0d0) - A * (2.0d0 * x(i) / yf - 2.0d0)
+    
+        else if(yf <= x(i) .and. x(i) < lam) then 
+          F(i) = B * (x(i) - yf) + C
+    
+        else if(lam <= x(i) .and. x(i) < l) then
+          F(i) = B * (x(i) - yf) - D
+        end if
+    
+      enddo
+
+    end function cal_Ffunc
+    
+end module Ffunc
+
+module Ae_calculation
+  implicit none
+  contains
+  
+    function Ae_cal(x, F, l, dn) Result(Ae)
+      integer, intent(in) :: dn
+      real(8), intent(in) :: x(0:dn), F(0:dn)
+      real(8), intent(in) :: l
+      real(8) Ae(0:dn), y, z, dy, sum
+      integer i, j
+
+      Ae(:) = 0.0
+      dy = l / dble(dn)
+
+      do i = 0, dn
+        sum = 0.0d0
+        do j = 0, i
+          y = dy * dble(j)
+          z = F(j) * sqrt(x(i)-y)
+
+          if (j == 0 .or. j ==i) then
+            sum = sum + 0.5d0 * z
+          else
+            sum = sum + z
+          end if
+        end do
+
+        Ae(i) = 4 * sum * dy
+      enddo
+
+    end function Ae_cal
+
+end module Ae_calculation
+
+
 program Ffunction
   use Ffunc
   use Ae_calculation
@@ -39,8 +108,8 @@ program Ffunction
   write(*,*) 'dn = ', dn
   
   !---Calculate F function---!
-  allocate (x(dn))
-  allocate (F(dn))
+  allocate (x(0:dn))
+  allocate (F(0:dn))
 
   x(:) = 0.0d0
   dx = l / dble(dn)
@@ -51,100 +120,40 @@ program Ffunction
 
   F(:) = cal_Ffunc(x, l, A, B, C, D, yf, lam, dn)
   write(*,*) 'calculation Ffunc... '
+  do i = 0, dn
+    write (*,*)i, x(i), F(i)
+  end do
 
   open(20, file='Darden Ffunc.txt')
 
-  write (20,*) 'i,   x(i),   F(i)'
+  write(20,*) 'i,   x(i),   F(i)'
     do i = 0, dn
-      write (20,*)i, x(i), F(i)
+      write(20,*) i, x(i), F(i)
     end do
 
   close(20)
   write(*,*) 'output Ffunc is completed !'
 
   !---Calculate Equivalent Area---!
-  allocate (Ae(dn))
+  allocate (Ae(0:dn))
 
   Ae(:) = Ae_cal(x, F, l, dn)
   write(*,*) 'calculation Ae... '
+  do i = 0, dn
+    write (*,*)i, x(i), Ae(i)
+  end do
 
   open(30, file='Equivalent Area Destribution.txt')
+  write(*,*) 'bug check'
 
   write (30,*) 'i,   x(i),   Ae(i)'
-    do i = 0, dn
-      write (30,*)i, x(i), Ae(i)
-    end do
+  do i = 0, dn
+    write(30,*) i, x(i), Ae(i)
+    write(*,*) 'wrote i =', i
+  end do
 
   close(30)
   write(*,*) 'output Ae is completed !'
   write(*,*) ' '
 
 end program Ffunction
-
-
-module Ffunc
-  implicit none
-  contains
-
-    function cal_Ffunc(x, l, A, B, C, D, yf, lam, dn) result(F)
-      integer, intent(in) :: dn
-      real(8), intent(in) :: x(dn)
-      real(8), intent(in) :: l, A, B, C, D, yf, lam 
-      real(8) F(dn), dx
-      integer i
-
-      F(:) = 0.0d0
-      dx = l / dble(dn)
-    
-      do i = 0, dn+1
-        if(x(i) <= (yf / 2.0d0)) then
-          F(i) = 2.0d0 * x(i) * A / yf
-        
-        else if((yf / 2.0d0) < x(i) .and. x(i) < yf ) then
-          F(i) = C * (2.0d0 * x(i) / yf - 1.0d0) - A * (2.0d0 * x(i) / yf - 2.0d0)
-    
-        else if(yf <= x(i) .and. x(i) < lam) then 
-          F(i) = B * (x(i) - yf) + C
-    
-        else if(lam <= x(i) .and. x(i) < l) then
-          F(i) = B * (x(i) - yf) - D
-        end if
-      enddo
-
-    end function cal_Ffunc
-
-end module Ffunc
-
-module Ae_calculation
-  implicit none
-  contains
-  
-    function Ae_cal(x, F, l, dn) Result(Ae)
-      integer, intent(in) :: dn
-      real(8), intent(in) :: x(dn), F(dn)
-      real(8), intent(in) :: l
-      real(8) Ae(dn), y, z, dy, sum
-      integer i, j
-
-      Ae(:) = 0.0
-      dy = l / dble(dn)
-      sum = 0.0d0
-
-      do i = 0, dn+1
-        do j = 0, i
-          y = dy * dble(j)
-          z = F(j) * sqrt(x(i)-y)
-
-          if (j == 0 .or. j ==i) then
-            sum = sum + 0.5d0 * z
-          else
-            sum = sum + z
-          end if
-        end do
-
-        Ae(i) = 4 * sum * dy
-      enddo
-
-    end function Ae_cal
-
-end module Ae_calculation
