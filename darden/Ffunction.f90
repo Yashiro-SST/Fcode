faef70964b77e5bee09e7e008adfb399d96f190c
Yashiro-SST <67620182+Yashiro-SST@users.noreply.github.com>
2020-11-30 18:28:41 +0900

    Update Ffunction.f90

--- darden/Ffunction.f90
+++ darden/Ffunction.f90
@@ -79,7 +79,7 @@ program Ffunction
 
   !---Variable definition---!
 
-  real(8) l, l_n, A, B, C, D, yf, lam
+  real(8) l, A, B, C, D, yf, lam
   real(8) dx
   real(8), allocatable :: x(:), F(:), Ae(:)
   integer dn, i
@@ -98,10 +98,8 @@ program Ffunction
 
   close(10)
 
-  l_n = l / l
 
   write(*,*) 'l = ', l
-  write(*,*) 'l_normalizaed', l_n
   write(*,*) 'A = ', A
   write(*,*) 'B = ', B
   write(*,*) 'C = ', C
@@ -115,12 +113,12 @@ program Ffunction
   allocate (F(0:dn))
 
   x(:) = 0.0d0
-  dx = l_n / dble(dn)
+  dx = l / dble(dn)
   do i = 0, dn
     x(i) = dx * dble(i)
   enddo
 
-  F(:) = cal_Ffunc(x, l_n, A, B, C, D, yf, lam, dn)
+  F(:) = cal_Ffunc(x, l, A, B, C, D, yf, lam, dn)
   write(*,*) 'calculation Ffunc... '
   do i = 0, dn
     write (*,*)i, x(i), F(i)
@@ -141,7 +139,7 @@ program Ffunction
   !---Calculate Equivalent Area---!
   allocate (Ae(0:dn))
 
-  Ae(:) = Ae_cal(x, F, l_n, dn)
+  Ae(:) = Ae_cal(x, F, l, dn)
   write(*,*) 'calculation Ae... '
   do i = 0, dn
     write(*,*) i, x(i), Ae(i)
